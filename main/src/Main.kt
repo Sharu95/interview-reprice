bfc5b6d93a8e2867f6c949756d965555631f001e
Sharanan Kulam <sharu95@users.noreply.github.com>
2026-05-12 01:05:28 +0200

    init solution

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -2,13 +2,26 @@ package main.kotlin
 
 import java.io.File
 
+/**
+ * Naive approach - iterate each word, and compare all other words
+ * Sorted strings for comparison, equality implies anagrams
+ */
+fun solution1(words: List<String>): List<String> = words.map { current ->
+    val currentSorted = current.toCharArray().sorted().joinToString("")
+    words.filter {
+        currentSorted == it.toCharArray().sorted().joinToString("")
+    }.joinToString(" ")
+}
+
 
 fun main() {
-    val words = File("eventyr.txt")
-        .readLines()
-        .filter(String::isNotEmpty)
+    val inputFile = File("eventyr.txt")
+    val outputFile = File("anagrams.txt")
 
+    val words = inputFile.readLines().filter(String::isNotEmpty)
+    val anagrams = solution1(words)
 
+    outputFile.writeText(anagrams.joinToString("\n"))
 
 
 }
