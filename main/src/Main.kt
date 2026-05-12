c8490b2efafb5146022b82a3c4644a34a6d0a468
Sharanan Kulam <sharu95@users.noreply.github.com>
2026-05-12 08:23:15 +0200

    refactor: update naive solution to only include anagrams as output

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -1,31 +1,37 @@
 package main.kotlin
 
 import java.io.File
+import kotlin.collections.map
 
 /**
  * Naive approach - iterate each word, and compare all other words
  * Sorted strings for comparison, equality implies anagrams
  */
-fun naiveSolution(words: List<String>): List<String> = words.map { current ->
+fun naiveSolution(words: List<String>): Map<String, List<String>> = words.associateWith { current ->
     val currentSorted = current.toCharArray().sorted().joinToString("")
     words.filter {
-        currentSorted == it.toCharArray().sorted().joinToString("")
-    }.joinToString(" ")
+        val otherSorted = it.toCharArray().sorted()
+        currentSorted == otherSorted.joinToString("") && current != it
+    }
+}.filter { it.value.isNotEmpty() }
+
+fun writeToFile(anagrams: Map<String, List<String>>, file: File) {
+    val output = anagrams
+        .map { "${it.key} ${it.value.joinToString(" ")}" }
+        .joinToString("\n")
+    file.writeText(output)
 }
 
+fun readFromFile(file: File) = file.readLines().filter(String::isNotEmpty)
 
 fun main() {
     val inputFile = File("eventyr.txt")
     val outputFile = File("anagrams.txt")
+    val answerFile = File("answers.txt") // Naive solution for verification
 
-    val words = inputFile.readLines().filter(String::isNotEmpty)
+    val words = readFromFile(inputFile)
 
     /* Native solution*/
-    val anagrams = naiveSolution(words)
-
-
-
-    outputFile.writeText(anagrams.joinToString("\n"))
-
-
+    val naiveSolutionAnagrams = naiveSolution(words)
+    writeToFile(naiveSolutionAnagrams, answerFile)
 }
